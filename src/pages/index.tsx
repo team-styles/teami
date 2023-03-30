71fb2aaedc7b5c2027746a115ad8370aa7fcac78
Aoki Takuma <takumakawauso4649@gmail.com>
2023-03-31 00:24:51 +0900

    環境構築をしました

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>teami</title>
+        <meta name="application-name" content="teami" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h2>teami</h2>
+      </main>
+
+      <footer></footer>
+    </div>
+  );
+};
+
+export default Home;
