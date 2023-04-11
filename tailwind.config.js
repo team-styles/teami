b06b64ca79cad0c964402ecfd3b14bc2a66e7752
Aoki Takuma <takumakawauso4649@gmail.com>
2023-04-11 13:04:06 +0900

    tailwindCSSが適用可能になりました

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: false,
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
